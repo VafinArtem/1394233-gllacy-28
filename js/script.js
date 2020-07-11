49f06e42e027c943d676e873315a6812a38baf72
Vafin Artem <a.vafin85@gmail.com>
2020-07-11 11:31:20 +0500

    улучшил скрипт слайдера

--- js/script.js
+++ js/script.js
@@ -72,41 +72,21 @@ let siteWrapper = document.querySelector(".site-wrapper")
 let sliderItems = document.querySelectorAll(".slider-item")
 let sliderButtons = document.querySelectorAll(".slider-toggle")
 
-sliderButtons[0].addEventListener("click", function(evt) {
-    evt.preventDefault();
-    siteWrapper.classList.remove("site-wrapper-2");
-    siteWrapper.classList.remove("site-wrapper-3");
-    siteWrapper.classList.add("site-wrapper-1");
-    sliderButtons[1].classList.remove("active-toggle");
-    sliderButtons[2].classList.remove("active-toggle");
-    sliderButtons[0].classList.add("active-toggle");
-    sliderItems[0].classList.add("slider-active")
-    sliderItems[1].classList.remove("slider-active")
-    sliderItems[2].classList.remove("slider-active")
-});
+for (let i = 0; i < sliderButtons.length; i++) {
+    sliderButtons[i].addEventListener('click', function() {
+        let currentButton = document.querySelector(".active-toggle");
+        currentButton.classList.remove("active-toggle");
+        sliderButtons[i].classList.add("active-toggle");
 
-sliderButtons[1].addEventListener("click", function(evt) {
-    evt.preventDefault();
-    siteWrapper.classList.remove("site-wrapper-1");
-    siteWrapper.classList.remove("site-wrapper-3");
-    siteWrapper.classList.add("site-wrapper-2");
-    sliderButtons[0].classList.remove("active-toggle");
-    sliderButtons[2].classList.remove("active-toggle");
-    sliderButtons[1].classList.add("active-toggle");
-    sliderItems[1].classList.add("slider-active")
-    sliderItems[0].classList.remove("slider-active")
-    sliderItems[2].classList.remove("slider-active")
-});
+        let currentSlider = document.querySelector(".slider-active")
+        currentSlider.classList.remove("slider-active");
+        sliderItems[i].classList.add("slider-active");
 
-sliderButtons[2].addEventListener("click", function(evt) {
-    evt.preventDefault();
-    siteWrapper.classList.remove("site-wrapper-1");
-    siteWrapper.classList.remove("site-wrapper-2");
-    siteWrapper.classList.add("site-wrapper-3");
-    sliderButtons[1].classList.remove("active-toggle");
-    sliderButtons[0].classList.remove("active-toggle");
-    sliderButtons[2].classList.add("active-toggle");
-    sliderItems[2].classList.add("slider-active")
-    sliderItems[0].classList.remove("slider-active")
-    sliderItems[1].classList.remove("slider-active")
-});
+        for (let n = 1; n <= sliderButtons.length; n++) {
+            if (siteWrapper.classList.contains("site-wrapper-" + n)) {
+                siteWrapper.classList.remove("site-wrapper-" + n);
+            };
+            siteWrapper.classList.add("site-wrapper-" + (i + 1));
+        };
+    });
+};
